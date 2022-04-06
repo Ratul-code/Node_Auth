562f657ad156267b0386600869ec4a987cabc3cc
Ratul <rafiratul01@gmail.com>
2022-04-06 23:55:37 +0600

    testing

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV==="production"){
 
 
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server starter at ${PORT}`);
 });
