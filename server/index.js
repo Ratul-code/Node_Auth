a95f19f154cf6acd5b94c87ae887103e9f7018fa
Ratul <rafiratul01@gmail.com>
2022-04-06 23:42:47 +0600

    testing

--- server/index.js
+++ server/index.js
@@ -25,16 +25,18 @@ app.use("/api/private", require("./Routes/private"));
 app.use(errorHandler);
 
 
-// if(process.env.NODE_ENV==="production"){
-//   app.use(express.static(path.join(__dirname,"/client/build")))
-//   app.get("*",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"client","build","index.html"))
-//   })
-// }
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"/client/build")))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
+  })
+}else{
+  app.get("/",(req,res)=>{
+    res.send("APP RUNNING OK sd")
+  })
+}
 
-app.get("/",(req,res)=>{
-  res.send("APP RUNNING OK sd")
-})
+
 
 
 const PORT = process.env.PORT || 8001;
