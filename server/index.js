cb857102f78cad7210f51db29a67aa358efb94f5
Ratul <rafiratul01@gmail.com>
2022-04-06 22:56:25 +0600

    testing

--- server/index.js
+++ server/index.js
@@ -25,12 +25,16 @@ app.use("/api/private", require("./Routes/private"));
 app.use(errorHandler);
 
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"/client/build")))
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"client","build","index.html"))
-  })
-}
+// if(process.env.NODE_ENV==="production"){
+//   app.use(express.static(path.join(__dirname,"/client/build")))
+//   app.get("*",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"client","build","index.html"))
+//   })
+// }
+
+app.get("/",(req,res)=>{
+  res.send("APP RUNNING")
+})
 
 
 const PORT = process.env.PORT || 8001;
